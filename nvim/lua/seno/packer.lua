28e8f5278477c4d60703969e1401b487885685ec
root <root@srv895981.hstgr.cloud>
2025-10-17 08:18:25 +0000

    add terminal

--- nvim/lua/seno/packer.lua
+++ nvim/lua/seno/packer.lua
@@ -37,4 +37,19 @@ return require('packer').startup(function(use)
 		'nvim-lualine/lualine.nvim',
 		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 	}
+
+	use {
+		'numToStr/FTerm.nvim',
+		config = function()
+			require('FTerm').setup({
+				border = 'double',
+				dimensions = {
+					height = 0.9,
+					width = 0.9,
+				},
+			})
+			vim.keymap.set('n', '<C-\\>', '<CMD>lua require("FTerm").toggle()<CR>')
+			vim.keymap.set('t', '<C-\\>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+		end
+	}
 end)
