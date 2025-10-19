9800bbddaf36247721d5954277c31a29e7f4699e
root <root@srv895981.hstgr.cloud>
2025-10-19 17:39:35 +0000

    add surround plugin

--- nvim/lua/seno/packer.lua
+++ nvim/lua/seno/packer.lua
@@ -5,39 +5,34 @@ vim.cmd [[packadd packer.nvim]]
 
 
 return require('packer').startup(function(use)
-
 	-- theme
 	use('Yazeed1s/minimal.nvim')
 	use 'wbthomason/packer.nvim'
-
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.8',
 		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
+		requires = { { 'nvim-lua/plenary.nvim' } }
 	}
 
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate"})
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 	use('mbbill/undotree')
-
 	-- LSP
 	use('neovim/nvim-lspconfig')
 	use('mason-org/mason.nvim')
 	use('mason-org/mason-lspconfig.nvim')
-
 	-- Autocompletion
-	use('hrsh7th/nvim-cmp')         -- Completion plugin
-	use('hrsh7th/cmp-nvim-lsp')     -- LSP completion source
-	use('hrsh7th/cmp-buffer')       -- Buffer completion source
-	use('hrsh7th/cmp-path')         -- Path completion source
-	use('hrsh7th/cmp-cmdline')      -- Command line completion
-
+	use('hrsh7th/nvim-cmp') -- Completion plugin
+	use('hrsh7th/cmp-nvim-lsp') -- LSP completion source
+	use('hrsh7th/cmp-buffer') -- Buffer completion source
+	use('hrsh7th/cmp-path') -- Path completion source
+	use('hrsh7th/cmp-cmdline') -- Command line completion
 	-- Snippets
-	use('L3MON4D3/LuaSnip',{
+	use('L3MON4D3/LuaSnip', {
 		-- follow latest release.
 		tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 		-- install jsregexp (optional!:).
 		run = "make install_jsregexp"
-	})         -- Snippet engine
+	})                       -- Snippet engine
 	use('saadparwaiz1/cmp_luasnip') -- Snippet completion source
 	use {
 		'nvim-lualine/lualine.nvim',
@@ -56,7 +51,13 @@ return require('packer').startup(function(use)
 		end
 	}
 	use('ThePrimeagen/vim-be-good')
+	use({
+		"kylechui/nvim-surround",
+		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
+	})
 end)
-
-
-
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -140,6 +140,12 @@ _G.packer_plugins = {
     path = "/root/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
+  ["nvim-surround"] = {
+    config = { "\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0" },
+    loaded = true,
+    path = "/root/.local/share/nvim/site/pack/packer/start/nvim-surround",
+    url = "https://github.com/kylechui/nvim-surround"
+  },
   ["nvim-treesitter"] = {
     loaded = true,
     path = "/root/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
@@ -183,6 +189,10 @@ time([[Defining packer_plugins]], false)
 time([[Config for FTerm.nvim]], true)
 try_loadstring("\27LJ\2\n\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\2B\0\2\1K\0\1\0\15dimensions\1\0\2\vheight\4Í™³æ\fÌ™³ÿ\3\nwidth\4Í™³æ\fÌ™³ÿ\3\1\0\2\vborder\vdouble\15dimensions\0\nsetup\nFTerm\frequire\0", "config", "FTerm.nvim")
 time([[Config for FTerm.nvim]], false)
+-- Config for: nvim-surround
+time([[Config for nvim-surround]], true)
+try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0", "config", "nvim-surround")
+time([[Config for nvim-surround]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
